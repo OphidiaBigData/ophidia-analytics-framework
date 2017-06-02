8db2fd0f8fca8d4d0c335fc68e7496f5558048ce
Cosimo Palazzo <cosimo.palazzo@cmcc.it>
2017-06-02 11:15:42 +0200

    Allow to hold values using OPH_MERGECUBES

--- include/drivers/OPH_MERGECUBES_operator.h
+++ include/drivers/OPH_MERGECUBES_operator.h
@@ -76,6 +76,7 @@
  * \param sessionid SessionID
  * \param id_user ID of submitter
  * \param description Free description to be associated with output cube
+ * \param hold_values Flag used to enable the copy of dimension values
  */
 struct _OPH_MERGECUBES_operator_handle {
 	ophidiadb oDB;
@@ -97,6 +98,7 @@ struct _OPH_MERGECUBES_operator_handle {
 	char *sessionid;
 	int id_user;
 	char *description;
+	char hold_values;
 };
 typedef struct _OPH_MERGECUBES_operator_handle OPH_MERGECUBES_operator_handle;
 
