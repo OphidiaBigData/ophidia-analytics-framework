5a90aa24b6f064c1a38731c4ac34d0f2455668e0
eldoo <eldoo@users.noreply.github.com>
2017-09-26 11:34:31 +0200

    Add driver utility procedures for datacube removal

---
+++ include/oph_driver_procedure_library.h
@@ -0,0 +1,45 @@
+/*
+    Ophidia Analytics Framework
+    Copyright (C) 2012-2017 CMCC Foundation
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef __OPH_DRIVER_PROC_H__
+#define __OPH_DRIVER_PROC_H__
+
+#include "config.h"
+
+#include "oph_ophidiadb_main.h"
+#include "oph_common.h"
+
+/**
+ * \brief Procedure used to delete all fragments associated to a datacube
+ * \param id_datacube Id of the datacube
+ * \param id_container Id of the container where the datacube belongs
+ * \param fragment_ids Contains the string of fragment relative index
+ * \return 0 if successfull, -1 otherwise
+ */
+int oph_dproc_delete_data(int id_datacube, int id_container, char *fragment_ids);
+
+/**
+ * \brief Procedure to remove all datacube information from OphidiaDB
+ * \param oDB Contains the parameters and the connection to OphidiaDB
+ * \param id_datacube Id of the datacube
+ * \param id_container Id of the container where the datacube belongs
+ * \return 0 if successfull, -1 otherwise
+ */
+int oph_dproc_clean_odb(ophidiadb *oDB, int id_datacube, int id_container);
+
+#endif				/* __OPH_DRIVER_PROC_H__ */
