ddb8a6a2dea4e5b49a284e668bb505d801bbe8a7
eldoo <eldoo@users.noreply.github.com>
2018-09-24 12:50:37 +0200

    Relax check on ncores when nthreads is 1

--- src/drivers/OPH_ROLLUP_operator.c
+++ src/drivers/OPH_ROLLUP_operator.c
@@ -345,7 +345,7 @@ int task_init(oph_operator_struct * handle)
 			goto __OPH_EXIT_1;
 		}
 		//Check that product of ncores and nthread is at most equal to total number of fragments        
-		if (((OPH_ROLLUP_operator_handle *) handle->operator_handle)->nthread * handle->proc_number > (unsigned int) tot_frag_num) {
+		if (((OPH_ROLLUP_operator_handle *) handle->operator_handle)->nthread > 1 && ((OPH_ROLLUP_operator_handle *) handle->operator_handle)->nthread * handle->proc_number > (unsigned int) tot_frag_num) {
 			oph_odb_cube_free_datacube(&cube);
 			pmesg(LOG_ERROR, __FILE__, __LINE__, "Number of cores per number of threads is bigger than total fragments\n");
 			logging(LOG_ERROR, __FILE__, __LINE__, ((OPH_ROLLUP_operator_handle *) handle->operator_handle)->id_input_container,
