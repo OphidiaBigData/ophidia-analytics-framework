73569cb405baa8cb9bf0181a86d8151f54a5cf2b
Cosimo Palazzo <cosimo.palazzo@cmcc.it>
2021-11-09 20:21:24 +0100

    Amend to previous commit

--- src/drivers/OPH_IMPORTESDM2_operator.c
+++ src/drivers/OPH_IMPORTESDM2_operator.c
@@ -937,6 +937,7 @@ int env_set(HASHTBL * task_tbl, oph_operator_struct * handle)
 		return OPH_ANALYTICS_OPERATOR_INVALID_PARAM;
 	}
 	//Check dimension names
+	int sub_to_dims[number_of_sub_dims];
 	for (i = 0; i < number_of_sub_dims; i++) {
 		dimname = sub_dims[i];
 		for (j = 0; j < ndims; j++)
@@ -951,34 +952,53 @@ int env_set(HASHTBL * task_tbl, oph_operator_struct * handle)
 				free(offset);
 			return OPH_ANALYTICS_OPERATOR_UTILITY_ERROR;
 		}
+		sub_to_dims[i] = j;
 	}
 
 	//Check the sub_filters strings
-	int tf = -1;		// Id of time filter
+	int natts = 0, tf = -1;	// Id of time filter
+	smd_attr_t *md, *current;
 	for (i = 0; i < number_of_sub_dims; i++) {
-		if (((OPH_IMPORTESDM2_operator_handle *) handle->operator_handle)->time_filter) {
-			if (tf >= 0) {
-				pmesg(LOG_ERROR, __FILE__, __LINE__, "Not more than one time dimension can be considered\n");
-				logging(LOG_ERROR, __FILE__, __LINE__, OPH_GENERIC_CONTAINER_ID, OPH_LOG_OPH_IMPORTESDM_INVALID_INPUT_STRING);
+		if (((OPH_IMPORTESDM2_operator_handle *) handle->operator_handle)->time_filter && (tf < 0)) {
+			// Let us assume that OPH_IN_PARAM_CALENDAR is only for time dimensions
+			if (!measure->dim_dataset[sub_to_dims[i]])
+				if ((esdm_dataset_open(measure->container, measure->dims_name[sub_to_dims[i]], ESDM_MODE_FLAG_READ, measure->dim_dataset + sub_to_dims[i]))) {
+					pmesg(LOG_ERROR, __FILE__, __LINE__, "Unable to read dimension information: type cannot be converted\n");
+					logging(LOG_ERROR, __FILE__, __LINE__, OPH_GENERIC_CONTAINER_ID, OPH_LOG_OPH_IMPORTESDM_DIM_READ_ERROR, "type cannot be converted");
+					oph_tp_free_multiple_value_param_list(sub_dims, number_of_sub_dims);
+					oph_tp_free_multiple_value_param_list(sub_filters, number_of_sub_filters);
+					if (offset)
+						free(offset);
+					return OPH_ANALYTICS_OPERATOR_INVALID_PARAM;
+				}
+			if ((esdm_dataset_get_attributes(measure->dim_dataset[sub_to_dims[i]], &md))) {
+				pmesg(LOG_ERROR, __FILE__, __LINE__, "Error recovering number of global attributes\n");
+				logging(LOG_ERROR, __FILE__, __LINE__, OPH_GENERIC_CONTAINER_ID, OPH_LOG_OPH_IMPORTESDM_NC_NATTS_ERROR);
 				oph_tp_free_multiple_value_param_list(sub_dims, number_of_sub_dims);
 				oph_tp_free_multiple_value_param_list(sub_filters, number_of_sub_filters);
 				if (offset)
 					free(offset);
 				return OPH_ANALYTICS_OPERATOR_INVALID_PARAM;
 			}
-			tf = i;
-			dimname = sub_dims[i];
-			for (j = 0; j < ndims; j++)
-				if (!strcmp(dimname, measure->dims_name[j]))
+			natts = md->children;
+			for (j = 0; j < natts; j++) {
+				current = smd_attr_get_child(md, j);
+				if (!strcmp(current->name, OPH_IN_PARAM_CALENDAR)) {
+					tf = i;
 					break;
-		} else if (strchr(sub_filters[i], OPH_DIM_SUBSET_SEPARATOR2) != strrchr(sub_filters[i], OPH_DIM_SUBSET_SEPARATOR2)) {
-			pmesg(LOG_ERROR, __FILE__, __LINE__, "Strided range are not supported\n");
-			logging(LOG_ERROR, __FILE__, __LINE__, OPH_GENERIC_CONTAINER_ID, OPH_LOG_OPH_IMPORTESDM_INVALID_INPUT_STRING);
-			oph_tp_free_multiple_value_param_list(sub_dims, number_of_sub_dims);
-			oph_tp_free_multiple_value_param_list(sub_filters, number_of_sub_filters);
-			if (offset)
-				free(offset);
-			return OPH_ANALYTICS_OPERATOR_INVALID_PARAM;
+				}
+			}
+		}
+		if ((tf != i) || !strchr(sub_filters[i], OPH_DIM_SUBSET_SEPARATOR[1])) {
+			if (strchr(sub_filters[i], OPH_DIM_SUBSET_SEPARATOR2) != strrchr(sub_filters[i], OPH_DIM_SUBSET_SEPARATOR2)) {
+				pmesg(LOG_ERROR, __FILE__, __LINE__, "Strided range are not supported\n");
+				logging(LOG_ERROR, __FILE__, __LINE__, OPH_GENERIC_CONTAINER_ID, OPH_LOG_OPH_IMPORTNC_INVALID_INPUT_STRING);
+				oph_tp_free_multiple_value_param_list(sub_dims, number_of_sub_dims);
+				oph_tp_free_multiple_value_param_list(sub_filters, number_of_sub_filters);
+				if (offset)
+					free(offset);
+				return OPH_ANALYTICS_OPERATOR_INVALID_PARAM;
+			}
 		}
 	}
 
